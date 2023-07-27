0a4a8e30f89af0d24e7a0ad4432e2ed990001498
vetrivelanarasu <vetrivelanarasu@gmail.com>
2023-07-28 01:15:22 +0530

    first commit

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const middlewares = jsonServer.defaults()
  
 server.use(middlewares)
 server.use('/api', router)
-server.listen(process.env.PORT || 5000, () => {
+server.listen(process.env.PORT || 3000, () => {
   console.log('JSON Server is running')
 })
